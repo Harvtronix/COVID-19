14fe4451112794ce8975cd380203bba7c1064e5f
Harvtronix Webmaster <webmaster@harvtronix.com>
2020-03-19 14:54:22 -0400

    updating live deployment manually

---
+++ precache-manifest.e99f8789bb99efd41f0bad2b82cd7795.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a82aaf5aa5c3f04733cd0e8bc2f20855",
+    "url": "/my-corona/index.html"
+  },
+  {
+    "revision": "68da8d957b6d284b867e",
+    "url": "/my-corona/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "12733b5de06b38d5e30f",
+    "url": "/my-corona/static/js/2.6c06f106.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/my-corona/static/js/2.6c06f106.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68da8d957b6d284b867e",
+    "url": "/my-corona/static/js/main.6377007b.chunk.js"
+  },
+  {
+    "revision": "b0ca09e01e7153c4e9d7",
+    "url": "/my-corona/static/js/runtime-main.a5cba33a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-corona/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2435f00f2fd501ef1acad84da23177c9.js"
+  "/my-corona/precache-manifest.e99f8789bb99efd41f0bad2b82cd7795.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-corona/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
