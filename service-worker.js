95364e6f8523aa16ceb772631497552ea272ae1d
GitHub Action <action@github.com>
2020-04-10 00:05:27 +0000

    Deploying to gh-pages from  @ 10483cc33815fb42cd3ac70e8f92d642e550504c 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.5e5669eeee8eccba22cc0a9926b7dc7f.js"
+  "./precache-manifest.2182bfa4484d9361a4ffdddb69509fdf.js"
 );
 
 self.addEventListener('message', (event) => {
